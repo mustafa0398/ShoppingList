f1cf14a97bea0cad93becfdcf5db2161b7be03ce
mustafa0398 <mustafa03oeztuerk@gmail.com>
2023-11-12 00:47:53 +0100

    Version 0.5

--- app/src/main/java/com/oeztuerk/shoppinglist/ShoppingListAdapter.java
+++ app/src/main/java/com/oeztuerk/shoppinglist/ShoppingListAdapter.java
@@ -2,7 +2,6 @@ package com.oeztuerk.shoppinglist;
 
 import android.content.Context;
 import android.content.DialogInterface;
-import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.view.LayoutInflater;
